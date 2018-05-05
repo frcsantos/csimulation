c0214e72d3b8cd12fd16d2e3439ae38db205679e
fe <fe@@gmail.com>
2018-05-05 00:12:56 -0300

    First

---
+++ simu.py
@@ -0,0 +1,12 @@
+#!user/bin/python
+
+import sys
+
+def help():
+	print "Provide number of total cells, number of mutated cells, division and death rate"
+
+def setup(ncells,div,death):
+	cpoll=dic{}
+	for i in range(1,ncells):
+			
+
